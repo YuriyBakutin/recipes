5f4d0c01fc4f18bb687d10af6e38e1d3772cf150
Yuriy Bakutin <yuriybakutin@gmail.com>
2025-01-29 09:13:19 +0300

    Доработка стилей и компоновки компонента SimpleButton

--- src/components/common/SimpleButton.vue
+++ src/components/common/SimpleButton.vue
@@ -6,28 +6,39 @@
       label: string
       disabled?: boolean
       fontBold?: boolean
+      labelFirst?: boolean
     }>(),
     {
       iconSize: 24,
       disabled: false,
       fontBold: true,
+      labelFirst: false,
     },
   )
 </script>
 <template>
   <div
     tabindex="0"
-    class="flex justify-end items-center gap-10"
+    class="flex items-center gap-10"
     :class="props.disabled ? 'text-inactive' : 'text-primary cursor-pointer'"
   >
+    <span
+      v-if="labelFirst"
+      class="underline hover:opacity-60 select-none"
+      :class="{ 'font-bold': props.fontBold, 'opacity-60': props.disabled }"
+    >
+      {{ props.label }}
+    </span>
     <Icon
       :name="iconName"
       :clickable="!props.disabled"
+      :class="props.disabled ? 'opacity-60' : 'hover:opacity-60'"
       :style="{ 'font-size': props.iconSize + 'px' }"
     />
     <span
-      class="underline hover:opacity-60"
-      :class="{ 'font-bold': props.fontBold }"
+      v-if="!labelFirst"
+      class="underline hover:opacity-60 select-none"
+      :class="{ 'font-bold': props.fontBold, 'opacity-60': props.disabled }"
     >
       {{ props.label }}
     </span>
